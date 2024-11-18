b1a6880cbc0eaf420b85cc5e821e34e9997bdefd
donaldjsexton <donaldjsexton@gmail.com>
2024-11-18 00:34:46 -0500

    Small Fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'donsex.dreamhosters.com/graphql',
+        hostname: 'donsex.dreamhosters.com/',
       },
     ],
   },
